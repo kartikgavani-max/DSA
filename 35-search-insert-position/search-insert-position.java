3aa05284acde44f0365cd65a80201eaee88f0d20
kartikgavani-max <kartikgavani@gmail.com>
2026-09-19 22:57:41 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (48.50%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int rigth = nums.length -1;
+
+        while(left <= rigth){
+
+            int mid = left + (rigth - left)/2;
+
+            if(nums[mid] == target){
+                 return mid;
+            }
+
+            else if(nums[mid]<target){
+                left = mid+1;
+            }
+
+            else{
+                rigth = mid-1;
+            }
+        }
+        return left;
+    }
+}
